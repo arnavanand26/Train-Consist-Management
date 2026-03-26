ef7d398b0d13f3d226aec64cdc9bef9fbfba7b31
arnavanand26 <aa4967@srmist.edu.in>
2026-03-26 15:10:58 +0530

    UC3

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,46 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Welcome Message
         System.out.println("=== Train Consist Management App ===");
 
-        // UC1: Initialize Train Consist
         List<String> trainConsist = new ArrayList<>();
         System.out.println("Train consist initialized.");
         System.out.println("Initial bogie count: " + trainConsist.size());
 
-        // ---------------- UC2 START ----------------
-
-        // Create Passenger Bogie List
         List<String> passengerBogies = new ArrayList<>();
 
-        // Add Passenger Bogies
         passengerBogies.add("Sleeper");
         passengerBogies.add("AC Chair");
         passengerBogies.add("First Class");
 
-        // Display Bogies after Addition
         System.out.println("\nPassenger bogies after addition:");
         System.out.println(passengerBogies);
 
-        // Remove a Bogie (AC Chair)
         passengerBogies.remove("AC Chair");
 
-        // Display after Removal
         System.out.println("\nPassenger bogies after removal:");
         System.out.println(passengerBogies);
 
-        // Check if Sleeper exists
         boolean exists = passengerBogies.contains("Sleeper");
         System.out.println("\nDoes Sleeper bogie exist? " + exists);
 
-        // Final State
         System.out.println("\nFinal passenger bogie list:");
         System.out.println(passengerBogies);
 
-        // ---------------- UC2 END ----------------
+        Set<String> bogieIds = new HashSet<>();
+
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG101");
+        bogieIds.add("BG103");
+
+        System.out.println("\nUnique Bogie IDs:");
+        System.out.println(bogieIds);
     }
 }
